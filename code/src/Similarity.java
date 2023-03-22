6e3b51344f58d32943e9ff1af49317801c6d7edd
Ezra Ford <ezraford44@gmail.com>
2023-03-21 18:44:08 -0700

    added data

--- code/src/Similarity.java
+++ code/src/Similarity.java
@@ -1,2 +1,21 @@
 public class Similarity {
+
+    /**
+     * @param stoplist is a list of stop words, one per line, that should be ignored from the input
+     * @param sentences is a list of sentences/text fragments, one per line, to be used for training the
+     *                  distributional similarity method.
+     * @param inputfile is a file consisting of lines of the following form:
+     *                  <word> <weighting> <sim_measure> (tab separated), where <weighting> is one of:
+     *                      – TF: term frequency - use the number of times each word occurs in the word context.
+     *                      – TFIDF: term frequency with inverse document frequency weighting – use the term
+     *                      frequency times the inverse document frequency. Calculate IDF using the <sentences>
+     *                      file treating each line as a separate document.
+     *                  and <sim measure> is one of:
+     *                      – L1: L1 distance, normalized by the L2 (Euclidean) length of the vectors.
+     *                      – EUCLIDEAN: Euclidean distance, normalized by the L2 (Euclidean) length of the vectors.
+     *                      – COSINE: Cosine distance, normalized by the L2 (Euclidean) length of the vectors.
+     */
+    public Similarity (String stoplist, String sentences, String inputfile) {
+        
+    }
 }
